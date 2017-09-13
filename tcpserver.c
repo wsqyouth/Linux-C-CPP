c5a9695297d654aa69cc0c8cb14dc662342c6553
泡泡 <2631520922@qq.com>
2017-09-13 22:00:43 +0800

    Update tcpserver.c
    与客户端一对一 交互  通信

--- tcpserver.c
+++ tcpserver.c
@@ -7,10 +7,16 @@
 #include <arpa/inet.h>  //inet_add
 #include <unistd.h>     //write
 
+#define  SERVER_PORT 8000
+#define  BUF_LEN     1024
 int main(int argc , char *argv[])
 {
 	int socket_fd, child_fd, ret;
 	struct sockaddr_in server_addr,client_addr;
+    
+	char Msg[100]={"Ok,i hear you!"};    
+	char buf[BUF_LEN] = {0};
+	int nbytes;
 
 	//create socket
 	socket_fd = socket(AF_INET , SOCK_STREAM ,0);
@@ -24,7 +30,7 @@ int main(int argc , char *argv[])
 	bzero(&server_addr,sizeof(struct sockaddr_in));
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	server_addr.sin_port = htons( 8000 );
+	server_addr.sin_port = htons( SERVER_PORT );
 	
 	//bind
 	if( bind(socket_fd,(struct sockaddr *)&server_addr ,sizeof(server_addr) ) <0)
@@ -50,9 +56,23 @@ int main(int argc , char *argv[])
 		}
         printf("Connect from %s:%u ...!\n",inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
 		
-		close(child_fd);
-	}
+		//read message
+		if(-1 == (nbytes = read(child_fd,buf,BUF_LEN)))
+		{
+        	 perror("Recv Error:");
+		     exit(1);
+    	}
+    	printf("Client arrived:%s    ,recv len=%d\n",buf,nbytes);
 
-	close(socket_fd);
+		//server reply message to client
+    	if(-1 == write(child_fd, Msg, strlen(Msg)) )
+		{
+	  	    perror("send msg Error:\n");
+      	    exit(1);
+		}
+		//close(child_fd);
+	}
+	//close
+	//close(socket_fd);
 	return 0;
 }
