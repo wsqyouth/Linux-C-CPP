f6fedba5d367f8badd081a6d58e765f42376abfc
泡泡 <2631520922@qq.com>
2017-09-28 11:23:50 +0800

    Create test_fork.c
    enhance how to use the function of fork(), and understand the pid number between parent and child~

---
+++ test_fork.c
@@ -0,0 +1,31 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/*
+* fork 被调用一次，返回值却有两次：
+*在父进程中，fork返回新创建子进程的进程ID。
+*在子进程中，fork返回0。
+*
+*gcc test_fork.c -o test_fork -lpthread
+*/
+int  main(void)
+{
+    pid_t pid;
+    pid = fork();
+    if(pid < 0)
+    {
+        printf("error in fork!");
+    }
+    else if(pid == 0)
+    {
+        printf("I am the child process, my process ID is %d,my parent ID is %d\n",getpid(),getppid());//子进程
+    }
+    else
+    {
+        printf("I am the parent process,the value of pid is %d\n",pid); //在父进程中，返回新创建子进程ID
+        printf("I am the parent process,my process ID is %d\n",getpid());
+    }
+
+    return 0;
+}
