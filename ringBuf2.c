a758cf597f83adcfe98ec6fee38b62ce36244a54
泡泡 <2631520922@qq.com>
2018-05-01 21:52:52 +0800

    Create ringBuf2.c

---
+++ ringBuf2.c
@@ -0,0 +1,169 @@
+
+//本代码参考https://www.cnblogs.com/xybaby/p/6516387.html#_label_7，实现两种方法的生产者消费者模型
+
+------
+基于资源信号量+互斥信号量
+#include <stdio.h>
+#include <pthread.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <semaphore.h>
+
+#define BUFF_SIZE  3
+#define PRODUCE_THREAD_SIZE 1
+int g_buff[BUFF_SIZE];
+int g_write_index = 0;
+int g_read_index = 0;
+
+sem_t lock;
+sem_t consume_sem, produce_sem;
+
+
+void* produce(void *ptr){
+    int idx = *(int*)ptr;
+    
+    while(1){
+        sem_wait(&produce_sem);// 限制了生产者并发的数目
+
+		sem_wait(&lock); // 对临界区的访问要加锁
+        g_buff[g_write_index] = idx;
+        g_write_index = (g_write_index + 1) % BUFF_SIZE;
+		printf("in produce %d ,write_index=%d read_index=%d\n",idx, g_write_index, g_read_index);
+        sem_post(&lock);
+
+        sem_post(&consume_sem);
+		sleep(1);
+    }
+    return NULL;
+}
+
+void* consume(void *ptr){
+    while(1){
+        sem_wait(&consume_sem);
+        sem_wait(&lock);
+        int data = g_buff[g_read_index];
+        g_buff[g_read_index] = -1;
+        g_read_index = (g_read_index + 1) % BUFF_SIZE;
+        printf("consume %d read_index=%d write_index=%d\n", data, g_read_index, g_write_index);
+        sem_post(&lock);
+        sem_post(&produce_sem);
+		sleep(1);
+    }
+    return NULL;
+}
+
+int main(int argc, char * argv[]){
+    pthread_t con;
+    pthread_t pros[PRODUCE_THREAD_SIZE];
+    sem_init(&lock, 0, 1);
+    sem_init(&consume_sem,0, 0);
+    sem_init(&produce_sem,0, BUFF_SIZE);
+
+    pthread_create(&con, NULL, consume, NULL);
+    int thread_args[PRODUCE_THREAD_SIZE];
+    for(int i = 0; i < PRODUCE_THREAD_SIZE; i++){
+        thread_args[i] = i + 1;
+        pthread_create(&pros[i], NULL, produce, (thread_args + i));
+    }
+
+    pthread_join(con,0);
+    for(int i = 0; i < PRODUCE_THREAD_SIZE; i++)
+        pthread_join(pros[i],0);
+
+    sem_destroy(&lock);
+    sem_destroy(&consume_sem);
+    sem_destroy(&produce_sem);
+
+    return 0;
+}
+
+
+
+------
+基于管程（互斥锁+条件变量）来实现
+https://blog.csdn.net/u010424605/article/details/42872189
+http://anzhsoft.iteye.com/blog/2031354
+
+#include <stdio.h>
+#include <pthread.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#define BUFF_SIZE  3
+#define PRODUCE_THREAD_SIZE 1
+int g_buff[BUFF_SIZE];
+int g_write_index = 0;
+int g_read_index = 0;
+
+pthread_mutex_t lock;
+pthread_cond_t consume_cond, produce_cond;
+
+
+void* produce(void *ptr){
+    int idx = *(int*)ptr;
+    
+    while(1){
+        pthread_mutex_lock(&lock);
+        if((g_write_index + 1)% BUFF_SIZE  == g_read_index)  //
+            pthread_cond_wait(&produce_cond, &lock);
+
+        g_buff[g_write_index] = idx;
+        g_write_index = (g_write_index + 1) % BUFF_SIZE;
+		printf("in produce %d ,write_index=%d read_index=%d\n",idx, g_write_index, g_read_index);
+        pthread_cond_signal(&consume_cond);
+        pthread_mutex_unlock(&lock);
+
+		sleep(1);
+
+    }
+    return NULL;
+}
+
+void* consume(void *ptr){
+    while(1){
+        pthread_mutex_lock(&lock);
+        if(g_read_index == g_write_index)
+             pthread_cond_wait(&consume_cond, &lock);
+
+        int data = g_buff[g_read_index];
+        g_buff[g_read_index] = -1;
+        g_read_index = (g_read_index + 1) % BUFF_SIZE;
+        printf("consume %d read_index=%d write_index=%d\n", data, g_read_index, g_write_index);
+
+        pthread_cond_signal(&produce_cond);
+        pthread_mutex_unlock(&lock);
+
+		sleep(1);
+    }
+    return NULL;
+}
+
+int main(int argc, char * argv[]){
+    pthread_t con;
+    pthread_t pros[PRODUCE_THREAD_SIZE];
+
+    srand((unsigned)time(NULL));
+    pthread_mutex_init(&lock, 0);
+    pthread_cond_init(&consume_cond,0);
+    pthread_cond_init(&produce_cond,0);
+
+    pthread_create(&con, NULL, consume, NULL);
+    int thread_args[PRODUCE_THREAD_SIZE];
+    for(int i = 0; i < PRODUCE_THREAD_SIZE; i++){
+        thread_args[i] = i + 1;
+        pthread_create(&pros[i], NULL, produce, (thread_args + i));
+    }
+
+    pthread_join(con,0);
+    for(int i = 0; i < PRODUCE_THREAD_SIZE; i++)
+        pthread_join(pros[i],0);
+
+    pthread_mutex_destroy(&lock);
+    pthread_cond_destroy(&consume_cond);
+    pthread_cond_destroy(&produce_cond);
+
+    return 0;
+}
