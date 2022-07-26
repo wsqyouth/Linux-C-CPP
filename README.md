06c5f60b5d2a8fcd87ba627cda15ddfa2293e1b5
wsqyouth <2631520922@qq.com>
2022-07-26 20:27:42 +0800

    feat: cpp set查找字符串
    Signed-off-by: wsqyouth <2631520922@qq.com>

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Linux-C/C++
+
 This is my exercises about learning C or C++ on Linux~
 
 tcpserver.c   One-to-one communication,Tcp server
+
+代码格式网址：http://www.jsons.cn/caddlanformat/
--- set_string_find.cpp
+++ set_string_find.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <set>
 #include <string>
@@ -6,11 +5,12 @@ using namespace std;
 int main() {
 	std::set<string> allow_post_set = {
 		"pre_auth_check/get",
-		"inquiry/get",
-		"keywords_lock_conflict/get",
-    "inquiry_execute/get",
-    "inquiry_record/get",
-	};
+				"inquiry/get",
+				"keywords_lock_conflict/get",
+		                "inquiry_execute/get",
+		                "inquiry_record/get",
+	}
+	;
 	string method = "inquiry_execute/get";
 	if(allow_post_set.find(method) !=allow_post_set.end()) {
 		cout << " found " << endl;
